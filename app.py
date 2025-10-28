02af6761d48e43ee0e38f6577fece51cace7d680
MD FIRDOSH ALAM <mdfirdoshalam851202@gmail.com>
2025-10-28 21:53:42 +0530

    Implement prediction endpoints in app.py
    Add GET and POST endpoints for patient prediction.

--- app.py
+++ app.py
@@ -14,6 +14,24 @@ def health():
     return {"version":version}
 
 
+# this resquest is important   
+@app.get("/predict")
+def predict_pacient(data:userinput):
+    input_df={
+     "age":data.age,
+     "service":data.service,
+     "arrival_year":data.arrival_year,
+     "arrival_month":data.arrival_month,
+     "arrival_day":data.arrival_day,
+     "departure_year":data.departure_year,
+     "departure_month":data.departure_month,
+     "departure_day":data.departure_day }
+
+    try:
+       prediction=predict_output(input_df)
+       return JSONResponse (status_code=200,content={"my predict satisfaction score":prediction})
+    except Exception as e:
+        return JSONResponse(status_code=404,content=str(e))
 #post requiest
 @app.post("/predict")
 def create_pred(data:userinput):
